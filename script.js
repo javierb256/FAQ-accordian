492c51613d988e1e7da73fb2058764a7427d7762
= <44247277+javierb256@users.noreply.github.com>
2024-11-08 21:17:52 -0800

    added accordion on 1 element

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 const question1 = document.querySelector('#question1');
 
-const img1 = './assets/images/icon-plus.svg';
+const img1 = question1.getElementsByTagName('img')[0].src;
 const img2 = './assets/images/icon-minus.svg';
 
 question1.addEventListener('click', (e)=> {
-    // const image = question1.getElementsByTagName('img')[0];
+    const image = question1.getElementsByTagName('img')[0];
     question1.children[1].classList.toggle('hidden');
+    // console.log(image)
+    console.log(image.src)
     // console.log(image.src)
     // console.log(image.src == img2)
     // image.src = img2
-    // image.src = image.src == img2 ? img1 : img2
+    // image.src = image.src == img1 ? img1 : img2
+    if(image.src == img1){
+        image.src = "./assets/images/icon-minus.svg"
+    } else {
+        image.src = "./assets/images/icon-plus.svg"
+    }
     
      
 })
