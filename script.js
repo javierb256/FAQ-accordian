fd9d5a13aaf842a6fa5e90d7edf4e6a770c2430a
= <44247277+javierb256@users.noreply.github.com>
2024-11-12 14:21:05 -0800

    finished mobile view

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-const accordion = document.querySelector('.accordion').children
-const img1 = question1.getElementsByTagName('img')[0].src;
-const img2 = './assets/images/icon-minus.svg';
+// select accordion element
+const accordion = document.querySelector(".accordion").children;
+const img1 = question1.getElementsByTagName("img")[0].src;
+const img2 = "./assets/images/icon-minus.svg";
 
-console.log(accordion)
+// go through accordion collection and apply onclick action on each Element
+for (let question of accordion) {
+  question.addEventListener("click", () => {
+    const image = question.getElementsByTagName("img")[0];
+    question.children[1].classList.toggle("hidden");
 
-for(let question of accordion){
-    question.addEventListener("click", ()=> {
-        const image = question.getElementsByTagName('img')[0]
-        question.children[1].classList.toggle("hidden");
-
-        if(image.src === img1){
-            image.src = img2
-        } else {
-            image.src = img1
-        }
-    })    
+    if (image.src === img1) {
+      image.src = img2;
+    } else {
+      image.src = img1;
+    }
+  });
 }
-
