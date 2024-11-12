6241a7186b11c3ce1f97942e14276d5037dadc4b
= <44247277+javierb256@users.noreply.github.com>
2024-11-12 12:20:48 -0800

    finished desktop view

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const accordion = document.querySelector('.accordion').children
 const img1 = question1.getElementsByTagName('img')[0].src;
 const img2 = './assets/images/icon-minus.svg';
 
+console.log(accordion)
+
 for(let question of accordion){
     question.addEventListener("click", ()=> {
         const image = question.getElementsByTagName('img')[0]
