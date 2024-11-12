91b5fbabc778a0e4694081335d56cce52f0568ad
= <44247277+javierb256@users.noreply.github.com>
2024-11-11 21:56:21 -0800

    finished accordion feature

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
-const question1 = document.querySelector('#question1');
-
+const accordion = document.querySelector('.accordion').children
 const img1 = question1.getElementsByTagName('img')[0].src;
 const img2 = './assets/images/icon-minus.svg';
 
-question1.addEventListener('click', (e)=> {
-    const image = question1.getElementsByTagName('img')[0];
-    question1.children[1].classList.toggle('hidden');
-    // console.log(image)
-    console.log(image.src)
-    // console.log(image.src)
-    // console.log(image.src == img2)
-    // image.src = img2
-    // image.src = image.src == img1 ? img1 : img2
-    if(image.src == img1){
-        image.src = "./assets/images/icon-minus.svg"
-    } else {
-        image.src = "./assets/images/icon-plus.svg"
-    }
-    
-     
-})
+for(let question of accordion){
+    question.addEventListener("click", ()=> {
+        const image = question.getElementsByTagName('img')[0]
+        question.children[1].classList.toggle("hidden");
+
+        if(image.src === img1){
+            image.src = img2
+        } else {
+            image.src = img1
+        }
+    })    
+}
+
